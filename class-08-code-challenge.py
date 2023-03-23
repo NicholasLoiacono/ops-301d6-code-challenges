c2e70abf182f62320be0e2a820e11926e80a9b5d
NicholasLoiacono <ntloiacono4@gmail.com>
2023-03-23 05:28:02 +0000

    Python List

---
+++ class-08-code-challenge.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+# Script: Ops 301d6 Class 08 Ops Challenge Solution
+# Author: Nicholas Loiacono
+# Date: 03/22/2023
+# Purpose: Python List
+# Resources: Chat GPT
+
+
+# Start
+
+my_list = ["apple", "banana", "cherry", "date", "elderberry", "fig", "grape", "honeydew", "imbe", "jackfruit"]
+
+print(my_list[3])  # prints the fourth element, "date"
+print(my_list[5:])  # prints the sixth through tenth element, ["fig", "grape", "honeydew", "imbe", "jackfruit"]
+
+my_list[6] = "onion"  # changes the value of the seventh element to "onion"
+
+print(my_list)  # prints the updated list with "grape" replaced with "onion"
+
+# End
